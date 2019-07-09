e530c8c97d9b202a6603939943b08d8bab3514d2
xiekong <345833303@qq.com>
2019-07-09 14:39:59 +0800

    Initial commit

---
+++ src/test/java/com/xiekong/scheduler/SolrDataImportSchedulerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiekong.scheduler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolrDataImportSchedulerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
